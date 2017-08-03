56a6b788407b8fc3a4cc8f822dc44aea3f113306
Jackie Chui <jshchui@gmail.com>
2017-08-02 19:16:15 -0700

    redid countdown and made everything better

--- functions/index.js
+++ functions/index.js
@@ -2,82 +2,82 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.startCycle = functions.database.ref('game-settings').onUpdate((event) => {
-  let cycleLoop;
-  // if game setting was returned TRUE, THEN RUN BELOW
-  if (event.data.val().started) {
-    // set up your game ONCE here
-    const cycle = {
-      cycleStart: Date.now(),
-      cycleEnd: Date.now() + (3 * 1000),
-      cycle: 'night'
-    };
-
-    // cycles through different states
-    const cycleMap = {
-      'night' : 'day',
-      // 'deaths' : 'day',
-      'day' : 'night'
-    }
-    // collect vote during night,
-    // on death , count votes, set dead on presence, clear votes.
-    // update cycle every 3 seconds
-    return event.data.ref.parent.child('presence').database.ref('/').child('react').update(cycle).then(() => {
-      cycleLoop = setInterval(() => {
-        console.log(new Date());
-        return event.data.ref.parent.child('game-settings').once('value').then((snap) => {
-          if (snap.val().started) {
-            event.data.ref.parent.child('presence').database.ref('/').child('react').once('value').then((newSnap) => {
-              const currentCycle = newSnap.val().cycle;
-              // UPDATE YOUR GAME EVERY CYCLE HERE
-              const newCycle = {
-                cycleStart: Date.now(),
-                cycleEnd: Date.now() + (3 * 1000),
-                cycle: cycleMap[currentCycle]
-              };
-              updateCycle(newCycle);
-            });
-          } else {
-            return clearInterval(cycleLoop);
-          }
-        });
-      }, 3 * 1000);
-
-      function updateCycle(newCycle) {
-        return event.data.ref.parent.child('presence').database.ref('/').child('react').update(newCycle);
-      }
-
-      // cycle: 'night' ? 'day' : 'night'
-      // isBlack ? {isHidden ? : 'foo' : 'bar '} : 'baz';
-
-      // var i = 0, times = 10;
-      // function f() {
-      //   console.log(Date.now())
-      //     event.data.ref.parent.child('game-settings').once('value').then((snap) => {
-      //       if (snap.val().started) {
-      //         event.data.ref.parent.child('presence').database.ref('/').child('react').once('value').then((newSnap) => {
-      //           const currentCycle = newSnap.val().cycle;
-      //           const newCycle = {
-      //             cylceStart: Date.now(),
-      //             cycleEnd: Date.now() + (3 * 1000),
-      //             cycle: 'night' ? 'day' : 'night'
-      //           };
-      //           updateCycle(newCycle);
-      //         });
-      //       }
-      //     });
-      //
-      //
-      //   i++ ;
-      //   if(i < times) {
-      //     setTimeout( f, 3000);
-      //   }
-      // }
-      //
-      // f();
-    });
-  }
-});
+// exports.startCycle = functions.database.ref('game-settings').onUpdate((event) => {
+//   let cycleLoop;
+//   // if game setting was returned TRUE, THEN RUN BELOW
+//   if (event.data.val().started) {
+//     // set up your game ONCE here
+    // const cycle = {
+    //   cycleStart: Date.now(),
+    //   cycleEnd: Date.now() + (3 * 1000),
+    //   cycle: 'night'
+    // };
+//
+//     // cycles through different states
+//     const cycleMap = {
+//       'night' : 'day',
+//       // 'deaths' : 'day',
+//       'day' : 'night'
+//     }
+//     // collect vote during night,
+//     // on death , count votes, set dead on presence, clear votes.
+//     // update cycle every 3 seconds
+//     return event.data.ref.parent.child('presence').database.ref('/').child('react').update(cycle).then(() => {
+//       cycleLoop = setInterval(() => {
+//         console.log(new Date());
+//         return event.data.ref.parent.child('game-settings').once('value').then((snap) => {
+//           if (snap.val().started) {
+//             event.data.ref.parent.child('presence').database.ref('/').child('react').once('value').then((newSnap) => {
+//               const currentCycle = newSnap.val().cycle;
+//               // UPDATE YOUR GAME EVERY CYCLE HERE
+//               const newCycle = {
+//                 cycleStart: Date.now(),
+//                 cycleEnd: Date.now() + (3 * 1000),
+//                 cycle: cycleMap[currentCycle]
+//               };
+//               updateCycle(newCycle);
+//             });
+//           } else {
+//             return clearInterval(cycleLoop);
+//           }
+//         });
+//       }, 3 * 1000);
+//
+//       function updateCycle(newCycle) {
+//         return event.data.ref.parent.child('presence').database.ref('/').child('react').update(newCycle);
+//       }
+//
+//       // cycle: 'night' ? 'day' : 'night'
+//       // isBlack ? {isHidden ? : 'foo' : 'bar '} : 'baz';
+//
+//       // var i = 0, times = 10;
+//       // function f() {
+//       //   console.log(Date.now())
+//       //     event.data.ref.parent.child('game-settings').once('value').then((snap) => {
+//       //       if (snap.val().started) {
+//       //         event.data.ref.parent.child('presence').database.ref('/').child('react').once('value').then((newSnap) => {
+//       //           const currentCycle = newSnap.val().cycle;
+//       //           const newCycle = {
+//       //             cylceStart: Date.now(),
+//       //             cycleEnd: Date.now() + (3 * 1000),
+//       //             cycle: 'night' ? 'day' : 'night'
+//       //           };
+//       //           updateCycle(newCycle);
+//       //         });
+//       //       }
+//       //     });
+//       //
+//       //
+//       //   i++ ;
+//       //   if(i < times) {
+//       //     setTimeout( f, 3000);
+//       //   }
+//       // }
+//       //
+//       // f();
+//     });
+//   }
+// });
 
 // checks if the presence is deleted then set game stetitng to false
 exports.endGame = functions.database.ref('presence').onDelete((event) => {
@@ -98,8 +98,47 @@ exports.startGame = functions.database.ref('presence').onUpdate((event) => {
       return snap.val()[playerID].ready;
     }).indexOf(false) === -1;
 
-    return event.data.ref.parent.child('game-settings').set({
-      started: allReady
-    });
+    if (allReady) {
+      return event.data.ref.parent.child('game-settings').set({
+        // started: allReady
+        gameState: "all-ready",
+        currentCounter: null
+      });
+    }
+
   });
 });
+
+exports.gameStateListener = functions.database.ref('game-settings').onUpdate((event) => {
+  const gameSettings = event.data.val()
+  const gameSettingsFirebaseObject = event.data.ref.parent.child('game-settings')
+
+  switch(gameSettings.gameState) {
+    case "all-ready":
+      gameStateAllReady(gameSettingsFirebaseObject)
+      break;
+    case "night":
+      console.log("night")
+      break;
+  }
+})
+
+const gameStateAllReady = (gameSettingsFirebaseObject) => {
+  let currentCountdown = 5
+
+  const int = setInterval(() => {
+    if(currentCountdown > 0) {
+      currentCountdown -= 1;
+      gameSettingsFirebaseObject.set({
+        currentCounter: currentCountdown
+      })
+    } else {
+      gameSettingsFirebaseObject.set({
+        gameState: "night",
+        currentCounter: null
+      })
+      return clearInterval(int)
+    }
+  }, 1000)
+
+}
--- src/App.js
+++ src/App.js
@@ -21,27 +21,9 @@ class App extends Component {
       thisplayerID: null,
       thisplayerRole: null,
       countDown: null
-      // time: {},
-      // seconds: 4,
-      // currentTime: 8
+
     };
-
-    // this.timer = 0;
-    // this.startTimer = this.startTimer.bind(this);
-    // this.countDown = this.countDown.bind(this);
   }
-
-  // secondsToTime(secs){
-  //   let divisor_for_minutes = secs % (60 * 60);
-  //   let minutes = Math.floor(divisor_for_minutes / 60);
-  //   let divisor_for_seconds = divisor_for_minutes % 60;
-  //   let seconds = Math.ceil(divisor_for_seconds);
-  //
-  //   let obj = {
-  //     "s": seconds
-  //   };
-  //   return obj;
-  // }
 
   componentDidMount() {
     const alias = prompt('What is your alias');
@@ -52,7 +34,7 @@ class App extends Component {
     const gameStateRef = rootRef.child('react').child('gameState');
     const reactdbStateRef = rootRef.child('react');
     // checks the cycle
-    const cycleStateRef = rootRef.child('react').child('cycle');
+    const gameSettingsRef = rootRef.child('game-settings')
     gameStateRef.set(false);
 
     //this.props from index.js
@@ -69,155 +51,34 @@ class App extends Component {
       });
     });
 
-    cycleStateRef.on('value', snap => {
+    gameSettingsRef.on('value', snap => {
+      const gameSettings = snap.val()
       this.setState({
-        gameStatus: snap.val() == 'night' ? 'night' : 'day'
+        countDown: gameSettings.currentCounter,
+        gameStatus: gameSettings.gameState
       })
-      // if(snap.val() == 'night') {
-      //   this.setState({
-      //     gameStatus: 'night'
-      //   });
-      // };
-      //
-      // if(snap.val() =='day') {
-      //   this.setState({
-      //     gameStatus: 'day'
-      //   });
-      // }
+      console.log("snap", gameSettings)
     })
-
     // reactdbStateRef.on('value', (snap) => {
+    //   clearInterval(int);
+    //   console.log('front-end-cycle-start')
     //   const gameStatus = snap.val();
     //
     //   let time = gameStatus.cycleEnd - gameStatus.cycleStart;
-    //   console.log(gameStatus.cycleEnd);
-    //   console.log(gameStatus.cycleStart);
+    //   console.log(time);
+    //   let theTimer = time/1000 + 1;
+    //
+      // const int = setInterval(() => {
+      //   if(theTimer > 0) {
+      //     theTimer -= 1;
+      //     this.setState({
+      //       countDown: theTimer
+      //     });
+      //   } else {
+      //     return clearInterval(int);
+      //   }
+      // }, 1000)
     // })
-
-
-    // reactdbStateRef.on('value', (snap) => {
-    //   const gameStatus = snap.val();
-    //
-    //   if (gameStatus.cycle !== this.state.cycle) {
-    //     const int = setInterval(() => {
-    //       let count = 4;
-    //       if (count > 0) {
-    //         count = count - 1;
-    //         console.log(count);
-    //         this.setState({
-    //           countDown: count
-    //         });
-    //       } else {
-    //         return clearInterval(int);
-    //       }
-    //     }, 1000)
-    //   }
-    //
-    //   // this.setState({
-    //   //   gameStatus: gameStatus
-    //   // })
-    // });
-
-
-  // .child('react').on('value', function(snap) => {
-  //   const gameStatus = snap.val()
-  //
-  //   if (gameStatus.cycle !== this.state.cycle) {
-  //     const int = setInterval(() => {
-  //       let count = 11;
-  //       if (count > 0) {
-  //         this.setState({
-  //           countDown: count -= 1
-  //         });
-  //       } else {
-  //         return clearInterval(int);
-  //       }
-  //     }, 1000)
-  //   }
-  //
-  //   this.setState({
-  //     gameStatus
-  //   });
-  // })
-  //
-  //
-  // {this.state.cycle}
-
-
-    //if gameStateRef on FIREBASE is true, set gameStatus state to ready
-    // gameStateRef.on('value', snap => {
-      // this is useless delete later;
-      // if(snap.val() == true) {
-      //   this.setState({
-      //     gameStatus: 'ready'
-      //   })
-      // };
-      //
-      // // COUNT DOWN BEGIN
-      // if (snap.val() == 'countdown') {
-      //   this.startTimer();
-      //   setTimeout(() => {
-      //     //refactor this later to firebaseService
-      //     firebase.database().ref().child('react').child('gameState').set('begin');
-      //   }, this.state.currentTime * 1000 + 1000);
-      // };
-      //
-      // //select from roster and gives the player the role from roster
-      // if(snap.val() == 'begin') {
-      //   var Roster = ['Villager', 'Werewolf', 'Seer'];
-      //   var Role = Roster[Math.floor(Math.random()*3)];
-      //   this.setState({
-      //     thisplayerRole: Role,
-      //     currentTime: 8
-      //   });
-      //   this.props.firebaseService.setPlayerRole(this.state.thisplayerID, Role);
-      //
-      //   // after giving out roles, set to NIGHTTIME
-      //
-      //   // Is there a better way to get this value?
-      //   let nightStatus;
-      //   let nightTrue = gameStateRef.on('value', (snapshot) => {
-      //     nightStatus = snapshot.val();
-      //     console.log('inside val:' + nightStatus);
-      //     return null;
-      //   });
-      //   // console.log('outside val:' + nightTrue);
-      //   // console.log('nightstatus:' + nightStatus);
-      //
-      //   // if(nightStatus == 'begin') {
-      //   // };
-      //
-      //   gameStateRef.set('night');
-      //
-      //
-      //   // let currentTime;
-      //   // let databaseTime = timeRef.on('value', (snapshot) => {
-      //   //   currentTime = snapshot.val();
-      //   //   return null;
-      //   // });
-      //
-      //   // this.timer = 0;
-      //   // this.startTimer();
-      // };
-      //
-      // // IT IS NIGHTTIME
-      // if(snap.val() == 'night') {
-      //   this.timer = 0;
-      //   this.startTimer();
-      //   console.log('it is night time');
-      //   setTimeout(() => {
-      //     //refactor this later to firebaseService
-      //     gameStateRef.set('day');
-      //   }, this.state.currentTime * 1000 + 1000);
-      // };
-      //
-      // // IT IS DAYTIME
-      // if(snap.val() == 'day') {
-      //   console.log('its day time');
-      // };
-
-    // });
-
 
     this.props.firebaseService.displayCurrentUser((currentUsers) => {
       this.setState({
@@ -226,181 +87,31 @@ class App extends Component {
       })
     });
 
-    // let timeLeftVar = this.secondsToTime(this.state.seconds);
-    // this.setState({ time: timeLeftVar })
-
   }// end of component did mount
 
-  //
-  // startTimer() {
-  //
-  //   if(this.timer == 0) {
-  //     // console.log('timer ran');
-  //
-  //     this.timer = setInterval(this.countDown, 1000);
-  //   }
-  //   firebase.database().ref().child('react').child('currentTime').set(this.state.currentTime);
-  // }
-
-
-  // countDown() {
-    // let currentTime = firebase.database().ref().child('react')
-    // .child('currentTime').once('value').then((snap) => {
-    //   console.log(snap.val());
-    //   return snap.val();
-    // });
-
-    // let timeRef = firebase.database().ref().child('react').child('currentTime');
-    // let currentTime = timeRef.on('value', function(snapshot) {
-    //   // console.log(snapshot.val() - 1);
-    //   const time = snapshot.val();
-    //   let timeMinus1 = snapshot.val() - 1;
-    //   return time;
-    // });
-
-    // let timeRef = firebase.database().ref().child('react').child('currentTime');
-    // let currentTime;
-    // timeRef.on('value', (snapshot) => {
-    //   let timeref = snapshot.val()
-    //   console.log(timeRef.key);
-    // });
-    // // why cant you just timeRef.key.val()?
-    //
-    // let minusOne = currentTime - 1;
-    // console.log(minusOne);
-    // if(minusOne >= 0) {
-    //   timeRef.set(minusOne);
-    // };
-    //
-    // this.setState({
-    //   time: this.secondsToTime(currentTime),
-    //   // seconds: seconds
-    //   currentTime: minusOne
-    // });
-    //
-    // if (currentTime == 0) {
-    //   clearInterval(this.timer);
-    // }
-  // }
-
-
-  componentDidUpdate() {
-    //Once status is true from the react true data, give out role
-    // if(this.state.gameStatus == 'ready') {
-    //   var Roster = ['Villager', 'Werewolf', 'Seer']
-    //   var Role = Roster[Math.floor(Math.random()*3)];
-    //
-    //   // This is not updateing the state, its not working!!!
-    //   this.setState({
-    //     thisplayerRole: Role,
-    //     gameStatus: 'begin'
-    //   });
-    //   console.log(Role);
-    //   console.log(this.state.thisplayerRole);
-    //   // TODO: WHY DOESN"T this.state.thisplayerRole work here
-    //   console.log(this.state.gameStatus);
-    //   firebase.database().ref().child('presence').child(this.state.thisplayerID).child('role').set(this.state.thisplayerRole);
-    // }
-  }
 
 
 
-  //maybe used for later
-  checkPlayerStatus() {
-
-  }
-
-
-  // maybe used for later
-  doSomething(playerID) {
-    // this.state.players
-    // this.state.players[playerID];
-    // console.log(playerID);
-  }
-
-
-
-
-  // loopThroughPlayers = () => {
-    // this.props.firebaseService.setReady(this.state.thisplayerID).then(
-      // (gameStart) => {
-      // console.log(`Gamestate: ${gameStart}`);
-      // if(gameStart == true) {
-        // this.startTimer();
-        // firebase.database().ref().child('react').child('gameState').set('countdown');
-        //once everyone is ready, start timer and set react gameState data to true
-        //which also sets gamesState state to begin
-        // setTimeout(() => {
-        //   //refactor this later to firebaseService
-        //   firebase.database().ref().child('react').child('gameState').set('begin');
-        // }, this.state.currentTime * 1000 + 1000);
-
-    //   }
-    // });
-    // var name = this.state.alias;
-    // console.log(this.state.alias);
-
-
-    //
-    // firebase.database().ref().child('presence').child(this.state.thisplayerID).child('ready').set(true);
-    //
-    // var query = firebase.database().ref('presence');
-    // query.once("value")
-    //   .then(function(snapshot) {
-    //     var gameReady = true;
-    //     snapshot.forEach(function(childSnapshot) {
-    //       var key = childSnapshot.key;
-    //       var childData = childSnapshot.val();
-    //       if (childData.ready == false) {
-    //         gameReady = false;
-    //       }
-    //     });
-    //     return gameReady;
-    //   }).then(function(gameReady) {
-    //     if(gameReady == true) {
-    //       console.log('Game Start');
-    //     }
-    //   })
-  // }
-
-  // <Role playerID={this.state.thisPlayerID} loopThroughPlayers={this.loopThroughPlayers}/>
-
-  // <button onClick={this.startTimer}>Start</button>
   onReadyUp = () => {
     return this.props.firebaseService.setReady(this.state.thisplayerID);
   }
 
   render() {
-    // const gameStatus = firebase.database().ref().child('react').child('gameState');
-    // console.log(this.state.thisplayerRole);
     let Timer = null; //Timer is for rendering out the timer below
-    // if(this.state.seconds <= 3) {
-      // Timer = <h3> Game Starting in: {this.state.time.s} </h3>;
     Timer = <h3> Game Starting in: {this.state.currentTime} </h3>;
-    // } else {
-      // Timer = null;
-    // }
-
-    // if( {this.state.gameStatus} == 'day') {
-    //   let color = 'lightyellow';
-    // } else {
-    //   let color = 'lightblue';
-    // }
-
-    //  loopThroughPlayers={this.loopThroughPlayers}
 
 
     return (
       <div className="App {this.state.gameStatus}">
-        {this.state.countDown}
         <div className="player-list">
           <PlayerList players={this.state.players} doSomething={this.doSomething} />
           <div>
+          <h2>{this.state.countDown}</h2>
             {Timer}
           </div>
           <div className="ready-role">
             <h2>Your role:{this.state.thisplayerRole}</h2>
-            <Role onReadyUp={this.onReadyUp} ready={this.ready}/>
+            <Role onReadyUp={this.onReadyUp} />
           </div>
 
         </div>
